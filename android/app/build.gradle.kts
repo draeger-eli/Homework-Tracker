6847a9aabf9dae68aebe767b5a40b1df247972c7
Eli Draeger <draeg041@crk.umn.edu>
2026-09-23 14:16:00 -0500

    Fixed duplicate Firebase dependencies and Gradle plugins block

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
-plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
-    
 }
 
 dependencies {
